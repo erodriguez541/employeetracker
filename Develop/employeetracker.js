33d725c8acf5f159aca97737111dc033e7d7251b
ErnieRodriguez <erod541@gmail.com>
2021-06-01 22:27:11 -0500

    added code for question prompts

--- Develop/employeetracker.js
+++ Develop/employeetracker.js
@@ -19,3 +19,47 @@ connection.connect((err) => {
   if (err) throw err;
   runSearch();
 });
+
+const runSearch = () => {
+    inquirer
+      .prompt({
+        name: 'action',
+        type: 'rawlist',
+        message: 'What would you like to do?',
+        choices: [
+          'Find songs by artist',
+          'Find all artists who appear more than once',
+          'Find data within a specific range',
+          'Search for a specific song',
+          'Find artists with a top song and top album in the same year',
+        ],
+      })
+      .then((answer) => {
+        switch (answer.action) {
+          case 'Find songs by artist':
+            artistSearch();
+            break;
+  
+          case 'Find all artists who appear more than once':
+            multiSearch();
+            break;
+  
+          case 'Find data within a specific range':
+            rangeSearch();
+            break;
+  
+          case 'Search for a specific song':
+            songSearch();
+            break;
+  
+          case 'Find artists with a top song and top album in the same year':
+            songAndAlbumSearch();
+            break;
+  
+          default:
+            console.log(`Invalid action: ${answer.action}`);
+            break;
+        }
+      });
+  };
+  
