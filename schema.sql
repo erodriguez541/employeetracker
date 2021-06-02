f8c765327dc693ce025027b2b937a64cbbdf6fca
ErnieRodriguez <erod541@gmail.com>
2021-06-01 22:12:15 -0500

    added role to the seed file

--- schema.sql
+++ schema.sql
@@ -4,8 +4,12 @@ USE employeetracker_db;
 
 --Departments
 CREATE TABLE departments(
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL,
     name VARCHAR(30),
     PRIMARY KEY (id)
 );
 
+--Role
+CREATE TABLE role (
+    id INT NOT NULL
+)
