215105a7727af16b5fb62e5f9c7d59f273481b71
ErnieRodriguez <erod541@gmail.com>
2021-06-01 22:17:47 -0500

    added js file for tracker

--- schema.sql
+++ schema.sql
@@ -11,5 +11,19 @@ CREATE TABLE departments(
 
 --Role
 CREATE TABLE role (
-    id INT NOT NULL
+    id INT NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL(10,2),
+    department_id INT (100),
+    PRIMARY KEY (ID)
+)
+
+--Employee
+CREATE TABLE employee(
+    id INT NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY (ID)
 )
